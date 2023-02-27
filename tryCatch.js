06039bd4ffef0d3905333bedfd5fb9dfc59672ed
Akash <akashks838@gamil.com>
2023-02-27 14:10:01 +0530

    sec

--- tryCatch.js
+++ tryCatch.js
@@ -1,20 +1,20 @@
+let container = document.getElementById("container");
+let data;
+
 async function getData() {
   try {
     let res = await fetch("https://fakestoreapi.com/products");
-    let data = await res.json();
+    data = await res.json();
     appendProducts(data);
-
-    console.log(data);
+    // console.log(data);
   } catch (err) {
     console.log(err);
   }
 }
 getData();
-
 // Append
-let container = document.getElementById("container");
-
 function appendProducts(data) {
+  container.innerHTML = null;
   data.forEach(function (ele) {
     let div = document.createElement("div");
     let img = document.createElement("img");
@@ -27,4 +27,18 @@ function appendProducts(data) {
     div.append(img, title, price);
     container.append(div);
   });
+}
+function sortLH() {
+  // console.log(data);
+  data = data.sort(function (a, b) {
+    return a.price - b.price;
+  });
+  appendProducts(data);
+}
+
+function sortHL() {
+  data = data.sort(function (a, b) {
+    return b.price - a.price;
+  });
+  appendProducts(data);
 }
